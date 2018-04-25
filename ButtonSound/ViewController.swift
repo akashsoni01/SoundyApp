10749cfb574f74010c1c24e92be9f2083ebccdca
admin <admin@admins-Mac-mini.local>
2018-04-24 23:03:18 -0700

    initial commit

---
+++ ButtonSound/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ButtonSound
+//
+//  Created by admin on 4/24/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
